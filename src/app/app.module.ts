012422ed2c27a0494eb7c7560dcb0733f34f9aa7
Jimmy <jimmy@wishingsoft.com>
2018-06-08 13:33:02 +0800

    init component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { TaskItemComponent } from './task-item/task-item.component';
+import { EditTaskComponent } from './edit-task/edit-task.component';
+import { AddTaskComponent } from './add-task/add-task.component';
+import { LayoutComponent } from './layout/layout.component';
+import { EditContentComponent } from './edit-content/edit-content.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TaskItemComponent,
+    EditTaskComponent,
+    AddTaskComponent,
+    LayoutComponent,
+    EditContentComponent
   ],
   imports: [
     BrowserModule
